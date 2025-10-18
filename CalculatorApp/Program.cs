0bf8686fbd72ce1cbd05a4366dc0c299c46180a7
naresh <pnaresh479@gmail.com>
2025-10-18 23:17:20 +0530

    first commit for this repo

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Simple .NET Calculator ===");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+, -, *, /): ");
+            char op = Console.ReadLine()[0];
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = op switch
+            {
+                '+' => num1 + num2,
+                '-' => num1 - num2,
+                '*' => num1 * num2,
+                '/' => num2 != 0 ? num1 / num2 : double.NaN,
+                _ => double.NaN
+            };
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
